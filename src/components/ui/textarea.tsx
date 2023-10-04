3ebba4513c1bd17ac8ddb8229f737fa2dbd38845
Kienoss <khuatbaokien09@gmail.com>
2023-10-04 18:32:10 +0700

    removed tsline ingore line

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { cn } from "src/lib/utils";
 
-// tslint:disable-next-line:no-empty-interface
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
