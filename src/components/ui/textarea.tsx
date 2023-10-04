c11382e61ee0adf8ed48ae222510a656da6a8973
Kienoss <khuatbaokien09@gmail.com>
2023-10-04 18:37:01 +0700

    added never to  fix no-empty-interface

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -3,7 +3,9 @@ import * as React from "react";
 import { cn } from "src/lib/utils";
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  never?: never;
+}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
